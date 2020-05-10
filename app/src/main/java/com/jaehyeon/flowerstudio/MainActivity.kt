c53818f470c8ad2abe6a6d92a6c62af81ba3d7ef
Jaehyeon <fourhf1@gmail.com>
2020-05-11 02:18:57 +0900

    customized

--- app/src/main/java/com/jaehyeon/flowerstudio/MainActivity.kt
+++ app/src/main/java/com/jaehyeon/flowerstudio/MainActivity.kt
@@ -16,11 +16,13 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+            R.id.navigation_home,
+            R.id.navigation_camera,
+            R.id.navigation_dictionary,
+            R.id.navigation_profile))
+
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/jaehyeon/flowerstudio/ui/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/jaehyeon/flowerstudio/ui/dictionary/DictionaryFragment.kt
@@ -1,23 +1,20 @@
-package com.jaehyeon.flowerstudio.ui.home
+package com.jaehyeon.flowerstudio.ui.dictionary
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.jaehyeon.flowerstudio.R
 
-class HomeFragment : Fragment() {
+class DictionaryFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_dictionary, container, false)
         return view
     }
 }
